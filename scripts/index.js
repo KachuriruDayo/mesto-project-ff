f71b414fb041f5860cc1c043bc97858dafe5c9ab
KachuriruDayo <arseniyvlasov2@gmail.com>
2023-12-11 21:45:11 +0300

    fix remove function

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,20 @@ const cardTemplate = document.querySelector('#card-template').content;
 // // @todo: DOM узлы
 const cardContener = document.querySelector('.places__list');
 // // @todo: Функция создания карточки
-function addCard(name, link) {
+function addCard(name, link, delCard) {
   const cardContent = cardTemplate.querySelector('.card').cloneNode(true);
   const delButton = cardContent.querySelector('.card__delete-button');
   cardContent.querySelector('.card__image').src = link;
   cardContent.querySelector('.card__image').alt = name;
   cardContent.querySelector('.card__title').textContent = name;
-  // @todo: Функция удаления карточки
-  delButton.addEventListener('click', function () {
-    const cardElement = delButton.closest('.card');
-    cardElement.remove();
-  }); 
+  delButton.addEventListener('click', delCard);
   return cardContent;
+}
+  // @todo: Функция удаления карточки
+function delCard (event) {
+  const cardElement = event.target.closest('.card');
+  cardElement.remove();
 }
 
 // // @todo: Вывести карточки на страницу
-initialCards.forEach(el => cardContener.append(addCard(el.name, el.link)));
+initialCards.forEach(el => cardContener.append(addCard(el.name, el.link, delCard)));
