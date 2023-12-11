e339ef514b2086eaf0bd5b43652ff65a82b5f25e
KachuriruDayo <arseniyvlasov2@gmail.com>
2023-12-11 21:20:12 +0300

    add js script (addCards and DelCards)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+// // @todo: Темплейт карточки
+const CardTemplate = document.querySelector('#card-template').content;
+// // @todo: DOM узлы
+const CardContener = document.querySelector('.places__list');
+// // @todo: Функция создания карточки
+function addCard(name, link) {
+  const CardContent = CardTemplate.querySelector('.card').cloneNode(true);
+  const delButton = CardContent.querySelector('.card__delete-button');
+  CardContent.querySelector('.card__image').src = link;
+  CardContent.querySelector('.card__image').alt = name;
+  CardContent.querySelector('.card__title').textContent = name;
+  // @todo: Функция удаления карточки
+  delButton.addEventListener('click', function () {
+    const CardElement = delButton.closest('.card');
+    CardElement.remove();
+  }); 
+  return CardContent;
+}
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// // @todo: Вывести карточки на страницу
+initialCards.forEach(el => CardContener.append(addCard(el.name, el.link)));
